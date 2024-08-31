7b74db99b80ffe329ea980993d20ac8e45d2c0bf
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-08-31 13:45:03 +0530

    added chart configuration

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,139 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import * as echarts from "echarts";
+
+const chart = ref(null); // chart reference
+
+function loadChart() {
+  var myChart = echarts.init(chart.value);
+  var option;
+
+  option = {
+    color: ["#80FFA5", "#00DDFF", "#37A2FF", "#FF0087", "#FFBF00"],
+    title: {
+      text: "",
+    },
+    tooltip: {
+      trigger: "axis",
+      backgroundColor: "#fff",
+      borderColor: "#ddd",
+      borderWidth: 1,
+      textStyle: {
+        color: "#333",
+      },
+      position: function (point, params, dom, rect, size) {
+        return [
+          point[0] - dom.offsetWidth / 2,
+          point[1] - dom.offsetHeight - 10,
+        ];
+      },
+      axisPointer: {
+        type: "none", // Remove the cross lines
+      },
+    },
+    legend: {
+      data: [],
+    },
+
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
+      height: "40%",
+    },
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        splitLine: {
+          show: false,
+        },
+        axisLine: {
+          show: false,
+        },
+        axisTick: {
+          show: false,
+        },
+      },
+    ],
+    yAxis: [
+      {
+        show: false,
+        type: "value",
+        splitLine: {
+          show: false,
+        },
+      },
+    ],
+    series: [
+      {
+        name: "Line 5",
+        type: "line",
+        stack: "Total",
+        smooth: true,
+        lineStyle: {
+          width: 2, // Line width
+          color: "#16A34A", // Line color
+        },
+        showSymbol: true, // Show symbols
+        symbol: "circle", // Symbol type
+        symbolSize: 10, // Symbol size
+        itemStyle: {
+          color: "#16A34A", // Green circle inside
+          borderColor: "rgba(255, 255, 255)", // Transparent white border
+          borderWidth: 4, // Border width for the circle
+        },
+        label: {
+          show: false, // Hide label on top of points
+        },
+        emphasis: {
+          focus: "series",
+          itemStyle: {
+            color: "#16A34A", // Same as default color
+            borderColor: "rgba(255, 255, 255)", // Transparent white border
+            borderWidth: 4, // Border width for hover state
+          },
+        },
+        areaStyle: {
+          opacity: 0.8,
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: "rgba(40, 168, 89, 0.2)", // 20% opacity
+            },
+            {
+              offset: 1,
+              color: "rgba(40, 168, 89, 0)", // 0% opacity
+            },
+          ]),
+        },
+        data: [220, 302, 181, 234, 210, 290, 150],
+      },
+    ],
+  };
+
+  option && myChart.setOption(option);
+}
+
+// hooks
+onMounted(() => {
+  loadChart();
+});
+</script>
 
 <template>
-  <div>Hello world</div>
+  <div id="main">
+    <div ref="chart" style="width: 800px; height: 500px"></div>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#main {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
